592764d6400e54a5ad1a59ff6b2262a16fe7b3b8
Tyler Bell <tbell2000@holyfamily.edu>
2025-10-18 14:40:23 -0400

    Debugged final output issues

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,81 @@
 class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
 
 
 
 class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+    def __init__(self):
+        self.root = None
 
+    def insert(self, parent_name, doctor_name, side):
+        '''
+        Inserts a new doctor node under a specific parent on the given side.
+        '''
+        new_node = DoctorNode(doctor_name)
+        parent = self._find(self.root, parent_name)
+        if not parent:
+            print(f"Parent '{parent_name}' not found.")
+            return
+
+        if side == "left":
+            parent.left = new_node
+        elif side == "right":
+            parent.right = new_node
+        else:
+            print("Side must be 'left' or 'right'.")
+
+    def _find(self, node, name):
+        '''
+        Recursively finds a node by name.
+        '''
+        if not node:
+            return None
+        if node.name == name:
+            return node
+
+        left_result = self._find(node.left, name)
+        if left_result:
+            return left_result
+        return self._find(node.right, name)
+
+    def preorder(self, node):
+        '''
+        Preorder traversal: root -> left -> right
+        '''
+        if not node:
+            return []
+        return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+
+    def inorder(self, node):
+        '''
+        Inorder traversal: left -> root -> right
+        '''
+        if not node:
+            return []
+        return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+
+    def postorder(self, node):
+        '''
+        Postorder traversal: left -> right -> root
+        '''
+        if not node:
+            return []
+        return self.postorder(node.left) + self.postorder(node.right) + [node.name]
 
 
 
 # Test your DoctorTree and DoctorNode classes here
+if __name__ == "__main__":
+    tree = DoctorTree()
+    tree.root = DoctorNode("Dr. Croft")
+    tree.insert("Dr. Croft", "Dr. Goldsmith", "right")
+    tree.insert("Dr. Croft", "Dr. Phan", "left")
+    tree.insert("Dr. Phan", "Dr. Carson", "right")
+    tree.insert("Dr. Phan", "Dr. Morgan", "left")
+
+    print(tree.preorder(tree.root))   # ["Dr. Croft", "Dr. Phan", "Dr. Morgan", "Dr. Carson", "Dr. Goldsmith"]
+    print(tree.inorder(tree.root))    # ["Dr. Morgan", "Dr. Phan", "Dr. Carson", "Dr. Croft", "Dr. Goldsmith"]
+    print(tree.postorder(tree.root))  # ["Dr. Morgan", "Dr. Carson", "Dr. Phan", "Dr. Goldsmith", "Dr. Croft"]
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,98 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
+    def __init__(self, name, urgency):
+        self.name = name
+        self.urgency = urgency
 
 
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = []
 
+    def _parent(self, index):
+        return (index - 1) // 2
 
+    def _left(self, index):
+        return 2 * index + 1
+
+    def _right(self, index):
+        return 2 * index + 2
+
+    def heapify_up(self, index):
+        while index > 0:
+            parent = self._parent(index)
+            if self.data[index].urgency < self.data[parent].urgency:
+                self.data[index], self.data[parent] = self.data[parent], self.data[index]
+                index = parent
+            else:
+                break
+
+    def heapify_down(self, index):
+        size = len(self.data)
+        while True:
+            left = self._left(index)
+            right = self._right(index)
+            smallest = index
+
+            if left < size and self.data[left].urgency < self.data[smallest].urgency:
+                smallest = left
+            if right < size and self.data[right].urgency < self.data[smallest].urgency:
+                smallest = right
+
+            if smallest != index:
+                self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+                index = smallest
+            else:
+                break
+
+    def insert(self, patient):
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def print_heap(self):
+        print("Current Queue:")
+        for p in self.data:
+            print(f"- {p.name} ({p.urgency})")
+
+    def peek(self):
+        if not self.data:
+            return None
+        return self.data[0]
+
+    def remove_min(self):
+        if not self.data:
+            return None
+        if len(self.data) == 1:
+            return self.data.pop()
+        root = self.data[0]
+        self.data[0] = self.data.pop()
+        self.heapify_down(0)
+        return root
 
 
 # Test your MinHeap class here including edge cases
+if __name__ == "__main__":
+    heap = MinHeap()
+    heap.insert(Patient("Jordan", 3))
+    heap.insert(Patient("Taylor", 1))
+    heap.insert(Patient("Avery", 5))
+    heap.print_heap()
+    print()
+
+    next_up = heap.peek()
+    if next_up:
+        print("Next up:", next_up.name, next_up.urgency)
+    print()
+
+    served = heap.remove_min()
+    if served:
+        print("Served:", served.name)
+    heap.print_heap()
+
+    print()
+    # Edge cases
+    print("Removing all remaining patients...")
+    while heap.data:
+        p = heap.remove_min()
+        print("Removed:", p.name)
+    print("Heap empty?", not heap.data)
